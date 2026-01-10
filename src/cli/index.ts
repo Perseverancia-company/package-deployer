732d318476909dfb1da041a46ccfcae26d1abbfe
Felix Riddle <fedecraft3312@gmail.com>
2026-01-10 15:57:20 -0300

    Changed cloning into a command

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -270,41 +270,48 @@ async function main() {
 		.command(
 			"repositories",
 			"Repositories",
-			(args) => {
-				return args
-					.option("use-whitelist", {
-						type: "boolean",
-						description: "Use the configuration whitelist",
-						default: false,
-					})
-					.option("clone-all", {
-						type: "boolean",
-						description: "Clone all repositories",
-					});
-			},
-			async (args) => {
-				// Use whitelist
-				const useWhitelist = args.useWhitelist;
+			(yargs: any) => {
+				return yargs.command(
+					"clone",
+					"Clone repositories",
+					(yargs: any) => {
+						return yargs
+							.option("use-whitelist", {
+								type: "boolean",
+								description: "Use the configuration whitelist",
+							})
+							.option("all", {
+								type: "boolean",
+								description: "Clone all the repositories",
+							});
+					},
+					async (args: any) => {
+						// Use whitelist
+						const useWhitelist = args.useWhitelist;
 
-				// Clone all the repositories
-				if (args.cloneAll) {
-					// Get(locally) or fetch(from github) repository list
-					const repositoryList = await RepositoryList.fromPath(
-						RepositoryList.defaultConfigurationFile(),
-						octokit
-					);
+						// Clone all the repositories
+						if (args.all) {
+							// Get(locally) or fetch(from github) repository list
+							const repositoryList =
+								await RepositoryList.fromPath(
+									RepositoryList.defaultConfigurationFile(),
+									octokit
+								);
 
-					// Clone all repositories
-					// They are processed in batchs internally
-					if (!useWhitelist) {
-						await repositoryList.cloneAll();
-					} else {
-						await repositoryList.cloneAll({
-							whitelist: config.getWhitelist(),
-						});
+							// Clone all repositories
+							// They are processed in batchs internally
+							if (!useWhitelist) {
+								await repositoryList.cloneAll();
+							} else {
+								await repositoryList.cloneAll({
+									whitelist: config.getWhitelist(),
+								});
+							}
+						}
 					}
-				}
-			}
+				);
+			},
+			async (args) => {}
 		)
 		.command(
 			"sync",
